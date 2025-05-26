fa3c6223be18d72c954fce98694eb2aa8699dba1
ZhixianHu <zhixianhu@link.cuhk.edu.cn>
2025-05-26 10:10:18 -0400

    250526 main functions update

---
+++ airflowCharacterization.py
@@ -0,0 +1,186 @@
+import cv2
+import numpy as np
+from scipy.fftpack import fft, ifft
+import copy
+import scipy.signal as signal
+from picamera2 import Picamera2
+import time
+import os
+from whiskerTrack import imgPreProcess, imgRedFindCentroid, imgRedCentroidTrack, rearrangeList
+
+def fftProcess(list, T, N = 30):
+    yf = fft(list)
+    xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
+    frequencies = np.fft.fftfreq(N, T)
+    magnitude_spectrum = np.abs(yf)[:N//2]
+    subFrequncies = frequencies[1:]
+    dominant_freq = subFrequncies[np.argmax(magnitude_spectrum)]
+    return xf, 2.0/N * np.abs(yf[0:N//2]), dominant_freq, magnitude_spectrum
+
+def flowProcess(dxL, dyL, dComplexL, dxPeakerFormer, dxValleyFormer, dyPeakerFormer, \
+                dyValleyFormer, T = 1/15, N=30, magThre = 4):
+    dxArray = np.array(dxL).T
+    dyArray = np.array(dyL).T
+    dComplexArray = np.array(dComplexL).T
+
+    staticFlag = [False]*8
+    contactFlag = [False]*8
+
+    def findPeakValley(array):
+        peakIndex, _ = signal.find_peaks(np.abs(array))
+        valleyIndex, _ = signal.find_peaks(-np.abs(array))
+        peak = array[peakIndex]
+        valley = array[valleyIndex]
+        if len(peak) == 0:
+            peakMax = array[0]
+        else:
+            peakMax = np.max(peak)
+        if len(valley) == 0:
+            valleyMin = array[0]
+        else:
+            valleyMin = np.min(valley)
+        return peakMax, valleyMin   
+    
+    dxPeakList = [0] * 8
+    dxValleyList = [0] * 8
+    dyPeakList = [0] * 8
+    dyValleyList = [0] * 8
+    for j in range(8):
+        xf, yf, dominant_freq, magnitude_spectrum = fftProcess(dComplexArray[j], T, N)
+        magnitude_spectrum = magnitude_spectrum[1:]
+        print(np.max(magnitude_spectrum))
+        if np.max(magnitude_spectrum) < 5:
+            staticFlag[j] = True
+        dxPeak, dxValley = findPeakValley(dxArray[j])
+        dyPeak, dyValley = findPeakValley(dyArray[j])
+    
+        
+        dxPeakList[j] = dxPeak
+        dxValleyList[j] = dxValley
+        dyPeakList[j] = dyPeak
+        dyValleyList[j] = dyValley
+
+        if abs(dxPeak - dxPeakerFormer[j]) > magThre:
+            contactFlag[j] = True
+        if abs(dxValley - dxValleyFormer[j]) > magThre:
+            contactFlag[j] = True
+        if abs(dyPeak - dyPeakerFormer[j]) > magThre:
+            contactFlag[j] = True
+        if abs(dyValley - dyValleyFormer[j]) > magThre:
+            contactFlag[j] = True
+    return staticFlag, contactFlag, dxPeakList, dxValleyList, dyPeakList, dyValleyList
+
+def dataSmooth(array, arrayFormer, thre):
+    for i in range(len(array)):
+        if abs(array[i] - arrayFormer[i]) <= thre:
+            array[i] = arrayFormer[i]
+    return array
+
+expID = 0
+expName ='airflow'
+
+date = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+outputFolder = 'data/' + expName + '_' + str(expID) + '_' + date + '/'
+os.makedirs(outputFolder, exist_ok=True)
+
+dxFlowDataList = []
+dyFlowDataList = []
+dComplexFlowDataList = []
+N = 15
+threshold = 1
+
+def main():
+    camera = Picamera2()
+    mode = camera.sensor_modes[0]
+    config = camera.create_preview_configuration(sensor={'output_size': mode['size'], \
+                                                        'bit_depth': mode['bit_depth']})
+    #config = camera.create_preview_configuration(main={"size":(640, 480)})
+    config["controls"]["FrameDurationLimits"] = (15000, 16677)
+    camera.configure(config)
+    camera.start()
+    
+    
+    orgImg = cv2.imread("img/org.png")
+    orgImg = imgPreProcess(orgImg)
+    orgRedMask, orgCenterPtList, orgRedFindFlag = imgRedFindCentroid(orgImg)
+    orgCenterPtList = rearrangeList(orgCenterPtList)
+    int_orgCenterPtList = [(int(pt[0]), int(pt[1])) for pt in orgCenterPtList]
+
+    k = 0
+    dxFormer = [0]*8
+    dyFormer = [0]*8
+    while k < N:
+        frame = camera.capture_array()
+        print("Captured")
+        r,g,b,dumb = cv2.split(frame)
+        img = cv2.merge([b,g,r])
+        img = imgPreProcess(img)
+
+        trkImg, org, centerPtList, dx, dy, int_centerPtL = imgRedCentroidTrack(img, orgCenterPtList, int_orgCenterPtList)
+        if dx == []:
+            continue
+        dx = dataSmooth(dx, dxFormer, threshold)
+        dy = dataSmooth(dy, dyFormer, threshold)
+        dxFormer = copy.deepcopy(dx)
+        dyFormer = copy.deepcopy(dy)
+
+        dxFlowDataList.append(dx)
+        dyFlowDataList.append(dy)
+        dComplexFlowDataList.append([dx[i] + 1j*dy[i] for i in range(8)])
+        k += 1
+        cv2.imshow("Track", trkImg)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    dxData = dxFlowDataList[-N:]
+    dyData = dyFlowDataList[-N:]
+    dComplexData = dComplexFlowDataList[-N:]
+    staticFlag, contactFlag, dxPeak, dxValley, dyPeak, dyValley = flowProcess(dxData, \
+        dyData, dComplexData, [0]*8, [0]*8, [0]*8, [0]*8)
+
+    count = 0
+    currTime = time.time()
+    while True:
+        frame = camera.capture_array()
+#         print("Captured")
+        r,g,b,dumb = cv2.split(frame)
+        img = cv2.merge([b,g,r])
+#         timestamp = time.time()
+#         image_filename = os.path.join(outputFolder, f"img_{count+30}_{timestamp:.5f}.jpg")
+#         cv2.imwrite(image_filename, img)
+        
+        img = imgPreProcess(img)
+        trkImg, org, centerPtList, dx, dy, int_centerPtL = imgRedCentroidTrack(img, orgCenterPtList, int_orgCenterPtList)
+        dxFlowDataList.append(dx)
+        dyFlowDataList.append(dy)
+        dComplexFlowDataList.append([dx[i] + 1j*dy[i] for i in range(8)])
+        
+        dxData = dxFlowDataList[-N:]
+        dyData = dyFlowDataList[-N:]
+        dComplexData = dComplexFlowDataList[-N:]
+        
+        dxSubData = dxFlowDataList[-2]
+        dySubData = dyFlowDataList[-2]
+
+        staticFlag, contactFlag, dxPeak, dxValley, dyPeak, dyValley = flowProcess(dxData, \
+            dyData, dComplexData, dxPeak, dxValley, dyPeak, dyValley)
+        count += 1
+
+        for i in range(8):
+            if staticFlag[i] or contactFlag[i]:
+                if ((centerPtList[i][0] - orgCenterPtList[i][0])**2+ \
+                    (centerPtList[i][1] - orgCenterPtList[i][1])**2) <= 13:
+                    cv2.putText(trkImg, "Origin", int_centerPtL[i], cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                else:
+                    cv2.putText(trkImg, "Contact", int_centerPtL[i], cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            else:
+                cv2.putText(trkImg, "Dynamic", int_centerPtL[i], cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+
+        cv2.imshow("Track", trkImg)
+        currTime = time.time()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+    cv2.destroyAllWindows()    
+main()
--- whiskerTrack.py
+++ whiskerTrack.py
@@ -2,5 +2,204 @@ import numpy as np
 import cv2
 import pandas as pd
 
-polygonMask = cv2.imread("polygonMask.png", cv2.IMREAD_GRAYSCALE)
+polygonMask = cv2.imread("img/polygonMask.png", cv2.IMREAD_GRAYSCALE)
 polygonMask = cv2.merge([polygonMask, polygonMask, polygonMask])
+
+# This function is used to reorder the points in the list
+def listReOrder(ptList):
+    if ptList[0][0] < ptList[1][0]:
+        mid = ptList[0]
+        ptList[0] = ptList[1]
+        ptList[1] = mid
+    if ptList[2][0] < ptList[3][0]:
+        mid = ptList[2]
+        ptList[2] = ptList[3]
+        ptList[3] = mid
+    if ptList[4][0] < ptList[5][0]:
+        mid = ptList[4]
+        ptList[4] = ptList[5]
+        ptList[5] = mid
+    if ptList[6][0] < ptList[7][0]:
+        mid = ptList[6]
+        ptList[6] = ptList[7]
+        ptList[7] = mid
+
+# This function is used to reorder the points in the list
+def rearrangeList(ptList):
+    newList = [None] * 8
+    newList[0] = ptList[0]
+    newList[1] = ptList[1]
+    newList[2] = ptList[3]
+    newList[3] = ptList[5]
+    newList[4] = ptList[7]
+    newList[5] = ptList[6]
+    newList[6] = ptList[4]
+    newList[7] = ptList[2]
+    return newList
+
+# This function is used to undistort the image
+def imgUndistort(frame, calibParaPath = "img/calibration_data.npz"):
+    calibPara = np.load(calibParaPath)
+    mtx = calibPara['mtx']
+    dist = calibPara['dist']
+    # rvecs = calibPara['rvecs']
+    # tvecs = calibPara['tvecs']
+    height, width = frame.shape[:2]
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (width, height), 1, (width, height))
+    frame = cv2.undistort(frame, mtx, dist, None, newcameramtx)
+    x, y, w, h = roi
+    frame = frame[y:y+h, x:x+w]
+    return frame
+
+def imgPreMaskProcess(frame, mask):
+    frame = cv2.bitwise_and(frame, mask)
+    return frame
+
+def imgPreProcess(frame):
+    frame = imgPreMaskProcess(frame, polygonMask)
+    frame = imgUndistort(frame)
+    return frame
+
+def imgRedFindCentroid(frame, minArea = 10, mskID = 0):
+    hsvImg = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    if mskID == 0:
+        mskRedThr1_low = np.array([0, 80, 60])
+        mskRedThr1_high = np.array([10, 255, 255])
+        mskRedThr2_low = np.array([170, 80, 60])
+        mskRedThr2_high = np.array([180, 255, 255])
+        
+        msk1 = cv2.inRange(hsvImg, mskRedThr1_low, mskRedThr1_high)
+        msk2 = cv2.inRange(hsvImg, mskRedThr2_low, mskRedThr2_high)
+        mask = msk1 + msk2
+        
+        kernel = np.ones((5,5),np.uint8)
+        erosion = cv2.erode(mask, kernel, iterations = 1)
+        dilation = cv2.dilate(erosion, kernel, iterations = 1)
+        mask = dilation
+    elif mskID == 1:
+        mskRedThr1_low = np.array([0, 80, 60])
+        mskRedThr1_high = np.array([10, 255, 255])
+        mskRedThr2_low = np.array([170, 80, 60])
+        mskRedThr2_high = np.array([180, 255, 255])
+        
+        msk1 = cv2.inRange(hsvImg, mskRedThr1_low, mskRedThr1_high)
+        msk2 = cv2.inRange(hsvImg, mskRedThr2_low, mskRedThr2_high)
+        mask = msk1 + msk2
+        
+        kernel = np.ones((5,5),np.uint8)
+        erosion = cv2.erode(mask, kernel, iterations = 1)
+        dilation = cv2.dilate(erosion, kernel, iterations = 2)
+        erosion = cv2.erode(dilation, kernel, iterations = 1)   
+        mask = erosion
+
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    filteredContours = []
+    centerPtList = []
+    for cnt in contours:
+        if cv2.contourArea(cnt) >= minArea:
+            filteredContours.append(cnt)
+            M = cv2.moments(cnt)
+            if M["m00"] != 0:
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+                centerPtList.append((cX, cY))
+
+    if len(filteredContours) != 8:
+        # print("-----!!! Contour detection failed.--------")
+        # print("contour: ", len(contours), "mskID: ", mskID)
+        centerPtList = []
+        # cv2.imshow("Img Red", mask)
+        # cv2.imshow('Original Frame', frame)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        if mskID == 1:
+            print("-----!!! Contour detection failed.--------")
+            print("contour: ", len(contours), "mskID: ", mskID)
+            return mask, centerPtList, False
+        return imgRedFindCentroid(frame, minArea, mskID+1)
+    # else:
+    #     cv2.imshow("Img Red", mask)
+    #     cv2.imshow('Original Frame', frame)
+    #     cv2.waitKey(2000)
+    # cv2.destroyAllWindows()
+
+    listReOrder(centerPtList)
+
+    # Visualize the result
+    # trkImg = frame.copy()
+    # redMask = np.zeros_like(mask, dtype=np.uint8)
+    # cv2.drawContours(redMask, filteredContours, -1, 255, thickness=cv2.FILLED)
+    # result = cv2.bitwise_and(frame, frame, mask=redMask)
+
+    # for i in range(8):
+    #     cv2.circle(trkImg, orgCenterPtList[i], 3, (255, 255, 255), -1)
+    #     cv2.circle(redMask, orgCenterPtList[i], 3, (0, 255, 0), -1)
+    # cv2.imshow("Track", trkImg)
+    # cv2.imshow("Img Red", mask)
+    # cv2.imshow('Original Frame', frame)
+    # cv2.imshow('Result', result)
+    # cv2.waitKey(0)
+    return mask, centerPtList, True
+
+orgImg = cv2.imread("img/org.png")
+orgImg = imgPreProcess(orgImg)
+
+orgRedMask, orgCenterPtList, orgRedFindFlag = imgRedFindCentroid(orgImg)
+orgCenterPtList = rearrangeList(orgCenterPtList)
+
+curveFitCenterPtList = [
+    (282, 297),  
+    (190, 287), 
+    (345, 233),
+    (130, 220), 
+    (353,  142), 
+    (135, 127), 
+    (295, 74),
+    (202, 66), 
+]
+curveFitCenterPtList = rearrangeList(curveFitCenterPtList)
+
+curve_fit_para = [
+    [  0.79557368, 282.3508395, 297.03295553,  -0.42777786],
+    [  0.89075305, 190.45936981, 286.96051867,  -0.35218606],
+    [  0.91560196, 345.01432358, 232.57820699,  -3.61919832],
+    [  0.85412267, 130.00728004, 219.92369247,  -0.24930196],
+    [  0.870223958, 353.435857,  142.422444, -0.334622223],
+    [  0.87530027, 134.67438286, 127.0337014,   -0.17724465],
+    [  0.830036046, 295.187737, 73.6946555, -0.218070369],
+    [  0.821017368, 202.121623, 66.0259798, 0.068201556]
+]
+curve_fit_para = rearrangeList(curve_fit_para)
+
+def imgRedCentroidTrack(frame, orgCenterPtL, imgPath, minArea = 10):
+    org = frame.copy()
+    trkImg = frame.copy()
+    redMask, centerPtList, redFindFlag = imgRedFindCentroid(frame, minArea)
+    centerPtList = rearrangeList(centerPtList)
+    int_orgCenterPtL = [(int(pt[0]), int(pt[1])) for pt in orgCenterPtL]
+    if not redFindFlag:
+        print(imgPath)
+        centerPtList = []
+        cv2.imshow("RedCentroid Track", trkImg)
+        cv2.imshow("RedCentroid Img Red", redMask)
+        cv2.imshow('RedCentroid Original Frame', frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        return frame, redMask, org, centerPtList, [], []
+    
+    mask = np.zeros_like(redMask, dtype=np.uint8)
+    dx = []
+    dy = []
+    for i in range(8):
+        cv2.circle(trkImg, int_orgCenterPtL[i], 3, (255, 255, 255), -1)
+        cv2.circle(trkImg, centerPtList[i], 3, (255, 255, 0), -1)
+        cv2.circle(mask, centerPtList[i], 3, (0, 255, 0), -1)
+        cv2.arrowedLine(trkImg, pt1=int_orgCenterPtL[i], pt2 = centerPtList[i], color=(0,255,255),\
+                        thickness = 2, line_type = cv2.LINE_8, shift=0, tipLength=0.5)
+        dx.append(centerPtList[i][0] - orgCenterPtL[i][0])
+        dy.append(centerPtList[i][1] - orgCenterPtL[i][1])
+    dx = np.array(dx)
+    dy = np.array(dy)
+    return frame, redMask, org, centerPtList, dx, dy
+
